58180a8deb3480a85803490a4326a84fdbb8d838
Paulmicha <paul.michalet@gmail.com>
2015-06-10 15:48:49 +0200

    Add Git snippet : Delete branches that are already merged

---
+++ Git/Delete_merged_branches.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# -*- coding: UTF8 -*-
+
+##
+#   Git snippets :
+#   Delete branches that are already merged
+#   
+#   Sources :
+#   http://stevenharman.net/git-clean-delete-already-merged-branches
+#   http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged
+#   
+
+#   Delete all branches that are already merged into the current branch
+git branch --merged | grep -v "\*" | xargs -n 1 git branch -d
+
+#   Delete all branches that are already merged into, for example, branch 'ANY-BRANCH'
+git branch --merged ANY-BRANCH | grep -v "\* ANY-BRANCH" | xargs -n 1 git branch -d
