84ce8957cd7853cfd47e9d184871a629bef6a9a5
Paulmicha <paul.michalet@gmail.com>
2016-08-14 15:22:18 -0300

    Update Git undo snippets.

--- Git/Undo.sh
+++ Git/Undo.sh
@@ -6,8 +6,13 @@
 #   
 #   Sources :
 #   http://stackoverflow.com/questions/953481/find-and-restore-a-deleted-file-in-a-git-repo
+#   http://superuser.com/questions/229290/how-to-amend-the-last-commit-to-un-add-a-file
 #   
 
 #   Restore file
 F='path/to/file'
 git checkout $(git rev-list -n 1 HEAD -- $F)^ -- $F
+
+#   Undo added files to last commit (not pushed yet).
+git reset HEAD^ path/to/accidentally/added/stuff
+git commit --amend
